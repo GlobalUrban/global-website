fb5f42404c0bf1d0771e8e86076ba8cbb8437d65
Edgardo Valencia <edgardo.avalencia@gmail.com>
2023-01-23 14:58:41 -0600

    Contact pop logic worked

--- dist/assets/js/phone-pop.js
+++ dist/assets/js/phone-pop.js
@@ -1,7 +1,10 @@
+
+
+// Phone Linsk || Call now & Get Directions //
 const mobileBtn = document.getElementById("contact-mobile");
 const contPopUp = document.getElementById("cont-pop-up");
 const popUp = document.getElementById("pop-up");
-let bool = false;
+
 
 mobileBtn.addEventListener("click", ()=> {
     contPopUp.classList.toggle("mobile-pop-up__show");
@@ -12,6 +15,25 @@ mobileBtn.addEventListener("click", ()=> {
 
 contPopUp.addEventListener("click", ()=> {
     contPopUp.classList.remove("mobile-pop-up__show");  
+})
+
+
+// Mobile Contact Form Pop up //
+// Phone Linsk || Call now & Get Directions //
+const contactBtn = document.getElementById("contact-form__mobile-btn");
+const contactContPopUp = document.getElementById("contact__pop-up");
+// const contactPopUp = document.getElementById("pop-up");
+
+
+contactBtn.addEventListener("click", ()=> {
+    contactContPopUp.classList.toggle("mobile-pop-up__show");
+    popUp.style.transform = "translateY(0%) translateX(0%)";
+    popUp.style.width = "90%";
+    popUp.style.height = "250px";
+})
+
+contactContPopUp.addEventListener("click", ()=> {
+    contactContPopUp.classList.remove("mobile-pop-up__show");  
 })
 
 
