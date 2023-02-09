c5898586444f1c13f9da61ad11cfc03cd87ee78e
frank <francortizm238@gmail.com>
2023-02-09 16:14:45 -0600

    logo communication 3 small made

--- dist/assets/js/about-us__team-slide.js
+++ dist/assets/js/about-us__team-slide.js
@@ -36,7 +36,7 @@ let teamMembers2 = [
     { name: 'Jackeline Landa', charge: 'Art Director' },
     { name: 'Kat Fuentes', charge: 'Creative Manager' },
     { name: 'Angie Mejia', charge: 'Senior Graphic Designer' },
-    { name: 'Don Hector', charge: 'Senior Web Developer' },
+    { name: 'Hector Suarez', charge: 'Senior Web Developer' },
     { name: 'Elena Mejia', charge: 'Social Media Strategist' },
     { name: 'Carolina Ramirez', charge: 'Motion Graphics & Press Production' },
     { name: 'Ximena Morazan', charge: 'Associate Social Media Strategist' },
