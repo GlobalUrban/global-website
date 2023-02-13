edf7ef8a3081b65f84113f6b11ceb87d821cdc18
Edgardo Valencia <edgardo.avalencia@gmail.com>
2023-02-13 14:54:30 -0600

    Portfolio Order fixed

--- dist/assets/js/portfolio_slide.js
+++ dist/assets/js/portfolio_slide.js
@@ -14,27 +14,27 @@ const projects1 = [
 
 const projects2 = [
     // Con tirro //
-    'WaterSmart Drought Response Program Grant <br/><span style="font-size:24px;">Grant Funded Amount: Pending</span> ',
-
-    'Equitable Communities Revitalization Grant <br/><span style="font-size:24px;">Grant Funded Amount: $6.85 million</span>',
-    "Children's Dental Care Program <br/><span style='font-size:24px;'>Grant Funded Amount: $8.4 million</span>",
-    'WaterSmart Drought Response Program Grant <br/><span style="font-size:24px;">Grant Funded Amount: Pending</span> ',
-    'Land and Water Conservation Grant <br/><span style="font-size:24px;">Grant Funded Amount: $6 million</span> ',
     'Safe Streets and Roads for All (SS4A) Grant <br/><span style="font-size:24px;">Grant Funded Amount: Pending</span>',
 
-    // Con tirro //
+    "Children's Dental Care Program <br/><span style='font-size:24px;'>Grant Funded Amount: $8.4 million</span>",
     'Equitable Communities Revitalization Grant <br/><span style="font-size:24px;">Grant Funded Amount: $6.85 million</span>',
+    'Safe Streets and Roads for All (SS4A) Grant <br/><span style="font-size:24px;">Grant Funded Amount: Pending</span>',
+    'Land and Water Conservation Grant <br/><span style="font-size:24px;">Grant Funded Amount: $6 million</span> ',
+    'WaterSmart Drought Response Program Grant <br/><span style="font-size:24px;">Grant Funded Amount: Pending</span> ',
+
+    // Con tirro //
+    "Children's Dental Care Program <br/><span style='font-size:24px;'>Grant Funded Amount: $8.4 million</span>",
 ]
 
 const projects3 = [
     // Con tirro //
-    'Pilot Interpretation Program <br/><h5>California Department of Water Resources</h5>',
+    'Delta Fuel Dump Crisis Management and Public Relations <br/><h5>City of Cudahy</h5>',
 
     'Virtual Meetings and Stakeholder Engagement <br/><h5>City of Oceanside</h5>',
     'Community Outreach <br/><h5>City of Montebello</h5>',
-    'Pilot Interpretation Program <br/><h5>California Department of Water Resources</h5>',
     'Delta Fuel Dump Crisis Management and Public Relations <br/><h5>City of Cudahy</h5>',
     'Proposition 68 Public Outreach and Community Engagement <br/><h5>City of Hawaiian Gardens</h5>',
+    'Pilot Interpretation Program <br/><h5>California Department of Water Resources</h5>',
 
     // Con tirro //
     'Virtual Meetings and Stakeholder Engagement <br/><h5>City of Oceanside</h5>',
@@ -42,13 +42,13 @@ const projects3 = [
 
 const projects4 = [
     // Con tirro //
-    'Graphic Design, Social Media, and Web Development <br/><h5>City of Oceanside</h5>',
+    'State of the City Presentation <br/><h5>City of San Luis Obispo</h5>',
 
     'Graphic Design and Social Media <br/><h5>City of El Monte</h5>',
     'Newsletter Design <br/><h5>City of Rosemead</h5>',
-    'Graphic Design and Social Media <br/><h5>City of El Monte</h5>',
     'State of the City Presentation <br/><h5>City of San Luis Obispo</h5>',
     'Branding, Graphic Design, and Events Stationery <br/><h5>California Contract Cities Association</h5>',
+    'Graphic Design and Social Media <br/><h5>City of La Puente</h5>',
 
     // Con tirro //
     'Graphic Design and Social Media <br/><h5>City of El Monte</h5>',
@@ -62,26 +62,26 @@ const d1 = [
     'Consequat reprehenderit dolor aute aute ullamco reprehenderit ad proident pariatur qui dolore. Fugiat sit amet mollit proident elit ullamco. Ipsum laborum enim nostrud sunt occaecat et ullamco dolor non pariatur consequat sint. In ut qui enim id irure aliquip. Ipsum tempor cillum qui nisi. Laboris ea labore proident culpa exercitation.',
     
     "Qui aliquip ullamco commodo exercitation et sit aliqua cupidatat duis est excepteur exercitation tempor deserunt. Do ea est sint excepteur proident nostrud occaecat. Et aute qui sit officia sit proident reprehenderit sunt. In esse mollit tempor elit qui. Ipsum minim est exercitation duis velit veniam minim commodo. Veniam commodo ad proident.",
-    
     'Consequat dolore id adipisicing pariatur. Adipisicing magna irure dolor ex anim. Velit eu magna velit eu veniam laboris anim amet nulla magna esse cupidatat ut cillum cupidatat. Excepteur aliqua consequat anim non exercitation sit ad exercitation enim consequat deserunt in consectetur velit laboris. Aute ea est magna culpa esse ipsum deserunt nisi quis reprehenderit.',
+
 
 
     'Consequat dolore id adipisicing pariatur. Adipisicing magna irure dolor ex anim. Velit eu magna velit eu veniam laboris anim amet nulla magna esse cupidatat ut cillum cupidatat. Excepteur aliqua consequat anim non exercitation sit ad exercitation enim consequat deserunt in consectetur velit laboris. Aute ea est magna culpa esse ipsum deserunt nisi quis reprehenderit.',
 ]
 
 const d2 = [
-    'Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem.',
+    `<span style='font-size:24px;'>Pasadena Water and Power Department</span> <br/><br/>Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
 
-
-    `<span style="font-size:24px; ">City of Lynwood</span><br/><br/> Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
 
     `<span style='font-size:24px;' >First 5 LA Commission</span> <br/><br/> Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
-
-    `<span style='font-size:24px;'>Pasadena Water and Power Department</span> <br/><br/>Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
+    
+    `<span style="font-size:24px; ">City of Lynwood</span><br/><br/> Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
+    
     
     `<span style='font-size:24px;'>City of Bell Gardens</span> <br/><br/>Our goal, along with the City of Bell Gardens, is to seek benefits for the community, which is why we create proposals for projects that can help the City. By working together, we get the best concessions for its residents. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
-    
     `<span style='font-size:24px;'>City of Azusa</span> <br/><br/>Working hand in hand with our clients is what we enjoy the most, and that's why we do our best for them, such as managing the CalHome Program and ensuring its residents' reach, effectiveness, and availability. Furthermore, we support the City by creating State and Federal funds proposals.<br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
+    
+    `<span style='font-size:24px;'>Pasadena Water and Power Department</span> <br/><br/>Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem. <br/><br/><h2 style="text-align:center;font-size:22px; color:rgba(241, 120, 56, 1);">Global has obtained over $750 million in funding for municipalities, agencies, organizations, and others.</h2>`,
 
 
     'Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem.',
@@ -95,11 +95,12 @@ const d3 = [
 
     "Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem.",
 
-    "We provided our Professional input to Stantec Consulting Services Inc. with a Service Plan to implement communication and engagement activities that expand the use of Written Translation Services, assisting Groundwater Sustainability Agencies and local water management groups, and creating interpretation services on a pilot scale.",
     
     "The key to public relations management is to support emergency crises at unnoticed times. Global Urban developed the strategic plan and executed responses in support of, and on behalf of, the City of Cudahy.",
     
     "Global assisted the City by preparing a three-day charrette workshop with the residents in order to develop a Community-Based Planning, where they engaged the community and obtained their input as to the types of recreational features that they wanted to see in their community. These meetings maximized the points allocated to the Proposition 68 project application that was submitted.",
+    
+    "We provided our Professional input to Stantec Consulting Services Inc. with a Service Plan to implement communication and engagement activities that expand the use of Written Translation Services, assisting Groundwater Sustainability Agencies and local water management groups, and creating interpretation services on a pilot scale.",
 
 
     'Duis sunt ullamco duis Lorem aute in voluptate minim commodo laborum culpa labore. Culpa esse cupidatat ea consequat do. Eiusmod dolore esse mollit ullamco incididunt consectetur Lorem culpa cillum est excepteur aliquip tempor. Commodo nisi exercitation consectetur. Occaecat consectetur dolore veniam est velit ea aliquip magna do eiusmod eiusmod culpa do enim Lorem.',
@@ -113,7 +114,7 @@ const d4 = [
 
     "Consectetur ullamco occaecat labore est cupidatat. Dolore cillum fugiat veniam cupidatat nulla aliquip eu ea amet sit. Aliqua incididunt sint cupidatat aute Lorem pariatur reprehenderit. Ea officia magna ut excepteur veniam sunt. Elit reprehenderit sit exercitation id nostrud ipsum deserunt id ullamco deserunt ex. Anim non incididunt exercitation deserunt ex. Do voluptate amet ad mollit duis dolor Lorem eiusmod. Cupidatat mollit do consequat ad duis nisi eiusmod consectetur elit non sit.",
 
-    "A website and social media are merged in design and content, which is why our team continues working to achieve the success of their social media and website, creating a cohesive graphic line that represents the image of the City of La Puente accurately.",
+    "Eiusmod Lorem incididunt laboris qui non do ad dolore quis proident irure ipsum cupidatat ea. Laborum anim nulla nisi ut consequat laboris labore eiusmod sint veniam incididunt voluptate enim. Cupidatat magna commodo tempor laboris magna. Commodo nulla aliquip deserunt eiusmod. Minim ex consequat non incididunt ullamco. Eu in sit nulla. Mollit eu ea dolore velit nostrud nulla laboris. Aute tempor velit officia adipisicing est laborum duis.",
     
     "A story is better when you can see it rather than imagine it. Our team prepared The State of the City 2022 Presentation for San Luis Obispo. Marketing material and highly produced videos were created to reflect the city's essence while informing its citizens about the improvements and new goals for their City.",
     
