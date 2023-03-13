a8b493705949a2dcf1b4d2d8df53d6b3514a0c1a
Edgardo Valencia <edgardo.avalencia@gmail.com>
2023-03-13 14:55:19 -0600

    Read more added to services paragraphs

--- dist/assets/js/services.js
+++ dist/assets/js/services.js
@@ -38,6 +38,8 @@ let Dlink2 = document.getElementById("d-services-2")
 let Dlink3 = document.getElementById("d-services-3")
 
 
+
+
 const setProjectToOpen = (service, project) => {
     window.localStorage.setItem("service", service)
     window.localStorage.setItem("project", project)
@@ -72,6 +74,47 @@ for (let i = 0; i < 4; i++) {
         servicesScroll.style.marginLeft = "0%";
     })
 }
+
+
+
+// Read More //
+function myFunction2() {
+    var dots2 = document.getElementById("dots2");
+    var moreText2 = document.getElementById("more2");
+    var btnText2 = document.getElementById("myBtn2");
+  
+    if (dots2.style.display === "none") {
+      dots2.style.display = "inline";
+      btnText2.innerHTML = "Read more";
+      moreText2.style.display = "none";
+    } else {
+      dots2.style.display = "none";
+      btnText2.innerHTML = "Read less";
+      moreText2.style.display = "inline";
+    }
+  } 
+  
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  } 
+
+
+
+
+
+   
 
 // // Program Management //
 // Plink1.addEventListener("click", () => { setProjectToOpen(0, 3) })
