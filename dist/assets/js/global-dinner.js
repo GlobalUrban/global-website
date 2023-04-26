4661500b8a71e69eb7b685c5c79145b16a01bcda
frank <francortizm238@gmail.com>
2023-04-26 12:24:09 -0600

    submit to integromat

---
+++ dist/assets/js/global-dinner.js
@@ -0,0 +1,20 @@
+let globalDinnerSubmitBtn = document.getElementById('global-dinner-submit')
+let globalDinnerForm = document.getElementById('global-dinner-form')
+// Form fields
+let globalDinnerFormName = document.getElementById('global-dinner-form-name')
+let globalDinnerFormEmail = document.getElementById('global-dinner-form-email')
+let globalDinnerFormPhone = document.getElementById('global-dinner-form-phone')
+let globalDinnerFormGuests = document.getElementById('global-dinner-form-guests')
+
+const handleDinnerSubmit = (e) => {
+    globalDinnerForm.submit();
+    e.stopPropagation();
+    e.preventDefault();
+    // setTimeout(() => {
+    //     window.location.href = '../html/global-dinner-invite.html'   
+        
+    // }, 300);
+}
+
+
+globalDinnerSubmitBtn.addEventListener('click', handleDinnerSubmit)
